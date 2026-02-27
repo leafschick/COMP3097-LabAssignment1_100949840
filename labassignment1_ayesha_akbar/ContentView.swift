45add99ecb98f1ab4f40162fd8ca23f10611630d
Ayesha Akbar <ayesha.akbar6@gmail.com>
2026-02-27 15:13:32 -0500

    make the prime button clickable

--- labassignment1_ayesha_akbar/ContentView.swift
+++ labassignment1_ayesha_akbar/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
     @State private var currentNumber: Int = 0
 
-    // Get a random number
     func generateRandomNumber() {
         currentNumber = Int.random(in: 1...100)
     }
@@ -20,25 +19,29 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+
             Text("\(currentNumber)")
                 .font(.system(size: 72, weight: .light))
                 .foregroundStyle(.teal)
-            
-            Text("Prime")
-                .font(.system(size: 32, weight: .light))
-                .foregroundStyle(Color.green)
-            
+
+            Button(action: {
+                
+            }) {
+                Text("Prime")
+                    .font(.system(size: 32, weight: .light))
+                    .foregroundStyle(Color.green)
+            }
+
             Text("Not Prime")
                 .font(.system(size: 32, weight: .light))
                 .foregroundStyle(Color.green)
-            
         }
         .padding()
         .onAppear {
             generateRandomNumber()
         }
-    }
-}
+    } // closes body
+} // closes struct
 
 #Preview {
     ContentView()
