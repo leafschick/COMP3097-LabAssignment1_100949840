f98051db42a487463c05f8d35c72454553d91cb9
Ayesha Akbar <ayesha.akbar6@gmail.com>
2026-02-27 14:15:43 -0500

    Initial Commit

---
+++ labassignment1_ayesha_akbar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  labassignment1_ayesha_akbar
+//
+//  Created by Ayesha Akbar on 2026-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ labassignment1_ayesha_akbar/labassignment1_ayesha_akbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  labassignment1_ayesha_akbarApp.swift
+//  labassignment1_ayesha_akbar
+//
+//  Created by Ayesha Akbar on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct labassignment1_ayesha_akbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
